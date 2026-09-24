c6cf6136119f0514c855c343f3d63cd755b5dea7
Syyedkhan <syyed480@gmail.com>
2026-09-24 07:35:44 +0530

    Add schengen passport option

--- script.js
+++ script.js
@@ -24,6 +24,7 @@ const examSpecs = {
     'us-visa': { photoW: 600, photoH: 600 },
     'uk-passport': { photoW: 413, photoH: 531 },
     'schengen': { photoW: 413, photoH: 531 },
+        'schengen-passport': { photoW: 413, photoH: 531, sigW: 0, sigH: 0, photoSize: 'Under 500 KB', sigSize: '-' },
     'canada-passport': { photoW: 591, photoH: 827 },
         'canada-visa': { photoW: 413, photoH: 531, sigW: 0, sigH: 0, photoSize: 'Under 500 KB', sigSize: '-' },
     'australia-passport': { photoW: 413, photoH: 531 }
